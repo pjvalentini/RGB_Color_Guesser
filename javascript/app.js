1f9546a35af6b50e3dde77765b441dcb319d7347
pjvalentini <pj.valentini@gmail.com>
2018-09-17 15:43:50 -0400

    adding first bit of JS

---
+++ javascript/app.js
@@ -0,0 +1,25 @@
+// Lets Start by giving our squares a dfferent color.
+
+// Create and array or random rgb colors.
+var colors = [
+    "rgb(255, 0, 0)", 
+    "rgb(0, 255, 0)", 
+    "rgb(0, 0, 255)",
+    "rgb(222, 34, 135)",
+    "rgb(44, 122, 5)",
+    "rgb(56, 244, 234)"
+];
+
+// Select all squares
+var squares = document.querySelectorAll(".square");
+// For now we hard code the correct color for testing.
+var pickedColor = colors[3];
+// select the span in the h1 to display the color to be guessed.
+var colorDisplay = document.getElementById("colorDisplay");
+// set pickedColor to display in the DOM
+colorDisplay.textContent = pickedColor;
+
+// loop through all squares and set colors on the them from the array
+for(var i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = colors[i];
+}
